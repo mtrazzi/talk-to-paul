77eb313ff2169e7f270e40e483a8f25aa0ec946e
Michael Trazzi <michael.trazzi@gmail.com>
2023-12-05 17:56:52 +0000

    first iteration of custom dataset

---
+++ llama-recipes-files/custom_dataset.py
@@ -0,0 +1,52 @@
+import copy
+import datasets
+import itertools
+
+import json
+from transformers import AutoTokenizer
+
+import random
+
+TRAIN_SPLIT = 0.8
+
+def tokenize_dialog(text, tokenizer):
+    text = text.replace('<eot>', '').replace('<eop>', '')
+    text_l = text.split('<eom>')[:-1] # nothing after the last <eom>
+
+    input_ids, labels = [], []
+    for text in text_l:
+        no_username = ':'.join(text.split(':')[:1])
+        tokens = tokenizer.encode(f"{tokenizer.bos_token} {no_username.strip()} {tokenizer.eos_token}", add_special_tokens=False)
+        input_ids += tokens
+        labels += tokens if 'Paul Christiano:' in text else [-100] * len(tokens)
+    return {
+        "input_ids": input_ids,
+        "labels": labels,
+        "attention_mask": [1] * len(input_ids)
+    }
+
+def get_custom_dataset(dataset_config, tokenizer, split):
+
+    jsonl_path = '/home/talk-to-paul/llama-recipes/examples/15M.jsonl'
+    print("[Dataset]: ", jsonl_path)
+
+    with open(jsonl_path, 'r') as file:
+        data = [json.loads(line) for line in file]
+
+    # shuffle the data
+    random.seed(0)
+    random.shuffle(data)
+
+    n = len(data)
+    if split == 'train':
+        data = data[:round(n * TRAIN_SPLIT)]
+    else:
+        data = data[round(n * TRAIN_SPLIT):]
+
+    # Tokenize each dialogue in the dataset
+    tokenized_data = []
+    for record in data:
+        tokenized_dialog = tokenize_dialog(record['text'], tokenizer)
+        tokenized_data.append(tokenized_dialog)
+
+    return tokenized_data
